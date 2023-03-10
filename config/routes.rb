77a426464384886d28479422bad1b38eb933ce0b
Iain McNulty <iain@inulty.com>
2023-03-10 13:17:00 +0000

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/show'
+  get 'signin', to: 'sessions#new', as: :signin
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy', as: :signout
+
   get 'signup', to: 'registrations#new'
   post 'signup', to: 'registrations#create'
 
