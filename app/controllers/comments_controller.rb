6aa02b890bab6ed94e8bf61f05b047bd8de8e7d6
Iain McNulty <iain@inulty.com>
2023-03-10 15:53:36 +0000

    Comments controller accepts body param

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -65,6 +65,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:project_id, :user_id)
+      params.require(:comment).permit(:project_id, :user_id, :body)
     end
 end
