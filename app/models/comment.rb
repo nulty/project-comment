67e30076172f2979fb3d829a1bdd612743933fa5
Iain McNulty <iain@inulty.com>
2023-03-11 14:29:14 +0000

    Activise Comment model   When a comment is created Tivity creates   an Activity record

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :project, required: true
   belongs_to :user, required: true
   validates :user, :project, presence: true
+  include Tivity::Activise
+
+  activities create: true
 end
--- lib/tivity.rb
+++ lib/tivity.rb
@@ -1,4 +1,6 @@
 module Tivity
+  require 'tivity/activise'
+
   def self.table_name_prefix
     'tivity_'
   end
